570e10758cf963699aadfa65cc715c8187bb8767
BattleTank <goldenspecops@gmail.com>
2018-07-12 02:43:29 -0700

    Add files via upload

---
+++ gradient_tests.py
@@ -0,0 +1,111 @@
+from __future__ import print_function
+import numpy as np
+from random import randrange
+
+EPSILON_UPDATE_FACTOR = 0.5
+
+
+def epsilon_update(epsilon):
+    return EPSILON_UPDATE_FACTOR*epsilon
+
+
+def gradient_test(f, f_gradient, x, epsilon0, num_iter=30, delta=0.1):
+    d = np.random.randn(*x.shape) * (2 / np.sqrt(np.size(x)))
+    total_err1 = 0
+    total_err2 = 0
+    flag = True
+    epsilon = epsilon0
+    grad = f_gradient(x) # grad by x requires trnapose here.... TODO
+    prev_value1 = np.linalg.norm(f(x+epsilon*d)-f(x))
+    prev_value2 = np.linalg.norm(f(x+epsilon*d)-f(x)-epsilon*np.dot(grad, epsilon*d.T))
+    for i in range(0, num_iter):
+        epsilon = epsilon_update(epsilon)
+        value1 = np.linalg.norm(f(x + epsilon * d) - f(x))
+        value2 = np.linalg.norm(f(x+epsilon*d)-f(x)-epsilon*np.dot(grad, epsilon*d.T))
+        total_err1 = total_err1+value1
+        total_err2 = total_err2+value2
+        print (["Gradient test iteration ", i, "  :  ", value1/prev_value1, value2/prev_value2])
+        if np.abs(value2/prev_value2 - EPSILON_UPDATE_FACTOR*EPSILON_UPDATE_FACTOR) > delta or np.abs(value1/prev_value1 - EPSILON_UPDATE_FACTOR) > delta:
+            flag = False
+        prev_value1 = value1
+        prev_value2 = value2
+    return total_err1,total_err2, flag
+
+
+def jacobian_test(f, f_jacobianmv, x, epsilon0, num_iter=30, delta=0.1):
+    d = np.random.randn(*x.shape) * (2 / np.sqrt(np.size(x)))
+    total_err1 = 0
+    total_err2 = 0
+    passed_all_tests = True
+    epsilon = epsilon0
+    prev_value1 = np.linalg.norm(f(x + epsilon * d) - f(x))
+    prev_value2 = np.linalg.norm(f(x + epsilon * d) - f(x) - f_jacobianmv(x, epsilon * d))
+    for i in range(0, num_iter):
+        epsilon = epsilon_update(epsilon)
+        value1 = np.linalg.norm(f(x + epsilon * d) - f(x), ord=2)
+        value2 = np.linalg.norm(f(x+epsilon*d)-f(x)-f_jacobianmv(x, epsilon*d), ord=2)
+        total_err1 = total_err1+value1
+        total_err2 = total_err2+value2
+        conv_rate_1 = value1 / prev_value1
+        conv_rate_2 = value2 / prev_value2
+        print(["Jacobian test iteration ", i, "  :  ", conv_rate_1, conv_rate_2])
+        if np.abs(conv_rate_2 - EPSILON_UPDATE_FACTOR*EPSILON_UPDATE_FACTOR) > delta or np.abs(conv_rate_1 - EPSILON_UPDATE_FACTOR) > delta:
+            passed_all_tests = False
+
+        prev_value1 = value1
+        prev_value2 = value2
+    return total_err1, total_err2, passed_all_tests
+
+
+def grad_check_sparse(f, x, analytic_grad, num_checks=10, h=1e-5):
+    """
+    sample a few random elements and only return numerical
+    in this dimensions.
+    """
+
+    total_err = 0
+
+    for i in range(num_checks):
+        ix = tuple([randrange(m) for m in x.shape])
+
+        oldval = x[ix]
+        x[ix] = oldval + h  # increment by h
+        fxph = f(x)  # evaluate f(x + h)
+        x[ix] = oldval - h  # increment by h
+        fxmh = f(x)  # evaluate f(x - h)
+        x[ix] = oldval  # reset
+
+        grad_numerical = (fxph - fxmh) / (2 * h)
+        grad_analytic = analytic_grad[ix]
+        rel_error = abs(grad_numerical - grad_analytic) / (abs(grad_numerical) + abs(grad_analytic))
+        total_err += rel_error
+        print('numerical: %f analytic: %f, relative error: %e' % (grad_numerical, grad_analytic, rel_error))
+
+    return total_err
+
+def grad_check_full(f, x, analytic_grad, num_checks=10, h=1e-3):
+    """
+    Experimental test...
+    """
+
+    total_err = 0
+
+    for i in range(num_checks):
+        ix = tuple([randrange(m) for m in x.shape])
+
+        oldval = x.copy()
+        x = oldval + h  # increment by h
+        fxph = f(x)  # evaluate f(x + h)
+        x = oldval - h  # increment by h
+        fxmh = f(x)  # evaluate f(x - h)
+        x = oldval  # reset
+
+        grad_numerical = (fxph - fxmh)
+        grad_analytic = np.sum(2*h*analytic_grad)
+        rel_error = abs(grad_numerical - grad_analytic) / (abs(grad_numerical) + abs(grad_analytic))
+        total_err += rel_error
+        print('numerical: %f analytic: %f, relative error: %e' % (grad_numerical, grad_analytic, rel_error))
+
+        h = h*0.5
+
+    return total_err
---
+++ main.py
@@ -0,0 +1,175 @@
+import numpy as np
+import tables
+import sys,os
+import scipy.io
+from softmax import Softmax, ResNetwork, train_with_sgd
+import matplotlib.pyplot as plt
+import itertools
+from gradient_tests import grad_check_full, grad_check_sparse, gradient_test, jacobian_test
+
+TEST_MODE = False
+
+
+def run_tests():
+    pass
+
+
+def generate_all_combinations(grid):
+    return itertools.product(*list(grid.values()))
+
+
+def run_tests(sample, label, num_labels):
+
+    ############# TODO REFACTOR!!!!!! ############
+    dim = sample.shape[1]
+
+    ###########  SOFTMAX TESTS  ############
+
+    demo_softmax = LonelySoftmaxWithReg(dim=dim, num_labels=num_labels, reg_param=0.1)
+    w = demo_softmax.get_params_as_matrix()
+
+    ## Test softmax gradient w.r.t X ##
+    softmax_val_x = lambda x: demo_softmax.calc_forward_pass(x, label)
+    softmax_grad_x = lambda x: demo_softmax.grad_by_x(x, label)  # returns (dL\dX)
+    gradient_test(softmax_val_x, softmax_grad_x, sample, 100, 50, 0.1)
+
+    ## Test softmax gradient w.r.t X numerically ##
+
+    grad = demo_softmax.grad_by_x(sample, label)
+    grad_err_1 = grad_check_full(softmax_val_x, sample, grad, 10)
+    grad_err_2 = grad_check_sparse(softmax_val_x, sample, grad, 10)
+    assert grad_err_2 < 0.01
+
+    ## Test softmax gradient w.r.t Params ##
+    softmax_val_w = lambda w: FunctionsBoxes.softmax_loss_and_gradient_regularized(w, demo_softmax.add_bias_dimension(sample), label, 0.1)[0]
+    softmax_grad_w = lambda w: FunctionsBoxes.softmax_loss_and_gradient_regularized(w, demo_softmax.add_bias_dimension(sample), label, 0.1)[1]
+    gradient_test(softmax_val_w, softmax_grad_w, w, 10, 20, 0.1)
+
+    ## Test softmax gradient w.r.t Params numerically ##
+
+    __, grad = demo_softmax.calc_loss_and_grad_for_batch(sample, label)
+    grad_err_1 = grad_check_full(softmax_val_w, demo_softmax.get_params_as_matrix(), grad, 10)
+    grad_err_2 = grad_check_sparse(softmax_val_w, demo_softmax.get_params_as_matrix(), grad, 10)
+    assert grad_err_2 < 0.01
+
+    ###########  RESNET LAYER TESTS  ############
+
+    demo_layer = ResLayer(dim=dim)
+    sample = sample.T  # For the layers, the data should be transposed
+
+    ## Test layer jacobian w.r.t X ##
+    layer_val_x = lambda x: demo_layer.calc_forward_pass(x)
+    layer_jacobian_vec_x = lambda x, v: demo_layer.calc_backward_pass(x, v)[-1]  # returns (dL\dX)^T . v
+    jacobian_test(layer_val_x, layer_jacobian_vec_x, sample, 100, 20, 0.1)
+
+
+    ## Test layer jacobian w.r.t Params ##
+
+    ## TODO
+
+
+def load_data(path):
+    f = scipy.io.loadmat(path)
+    t_data = f.get('Yt').T
+    t_labels = np.argmax(f.get('Ct'), axis=0)
+    v_data = f.get('Yv').T
+    v_labels = np.argmax(f.get('Cv'), axis=0)
+    num_labels = f.get('Cv').shape[0]
+
+    return t_data, t_labels, v_data, v_labels, num_labels
+
+
+def main(tests=False):
+
+    PATH = os.getcwd()+'\datasets\GMMData.mat'
+    t_data, t_labels, v_data, v_labels, num_labels = load_data(PATH)  # Data is of shape MxN where N is dim, M is # of samples
+
+    # Normalize the data
+    mean_image = np.mean(t_data, axis=0)
+    t_data -= mean_image
+    v_data -= mean_image
+    t_data /= np.std(t_data, axis=0)
+    v_data /= np.std(v_data, axis=0)
+
+    if tests:
+        run_tests(sample=v_data[None, 0], label=v_labels[None, 0], num_labels=num_labels)
+
+    #print(np.std(t_data, axis=0), np.mean(t_data, axis=0))  # Expect variance = 1, mean = 0
+
+    hyperparams_grid = {
+        "max_iter": [50],
+        "batch_size": [200],
+        "learning_rate": [0.001],
+        "decay_rate": [0.1],
+        "convergence_criteria": [0.01],
+        "gamma": [0.8],
+        "reg_param": [0.9],
+        "num_layers": [2]
+    }
+
+    max_acc = 0
+    cur_acc = 0
+    best_params = {}
+
+    for hp_comb in generate_all_combinations(hyperparams_grid):
+        hyperparams = {key: hp_comb[i] for key, i in zip(list(hyperparams_grid.keys()), range(len(hyperparams_grid.keys())))}
+
+        cur_acc = run_unit(t_data, t_labels, v_data, v_labels, num_labels, **hyperparams)
+        if cur_acc > max_acc:
+            best_params = hyperparams
+            max_acc = cur_acc
+
+    print("Maximal accuracy of %d on validation set, achieved with : %s" % (max_acc, str(best_params)))
+
+
+def run_unit(t_data, t_labels, v_data, v_labels, num_labels, **hyperparams):
+
+    print("Running unit with: " + str(hyperparams))
+    # Hyper parameters
+    MAX_ITER = hyperparams['max_iter']
+    BATCH_SIZE = hyperparams['batch_size']
+    LEARNING_RATE = hyperparams['learning_rate']
+    DECAY_RATE = hyperparams['decay_rate']
+    CONVERGENCE_CRITERIA = hyperparams['convergence_criteria']
+    GAMMA = hyperparams['gamma']
+    REG_PARAM = hyperparams['reg_param']
+    NUM_LAYERS = 2
+    if 'num_layers' in hyperparams.keys():
+        NUM_LAYERS = hyperparams['num_layers']
+
+    #model = Softmax(dim=t_data.shape[1], num_labels=num_labels, reg_param=REG_PARAM)
+
+    model = ResNetwork(NUM_LAYERS, t_data.shape[1], REG_PARAM, num_labels)
+
+    loss_history, accuracy_history = train_with_sgd(model, t_data=t_data, t_labels=t_labels, convergence_criteria=CONVERGENCE_CRITERIA,
+                                                    decay_rate=DECAY_RATE,
+                                                    batch_size=BATCH_SIZE,
+                                                    max_iter=MAX_ITER,
+                                                    learning_rate=LEARNING_RATE,
+                                                    gamma=GAMMA,
+                                                    v_data=v_data,
+                                                    v_labels=v_labels)
+
+    #print(loss_history)
+    # predictions = sm.predict(v_data)
+    iterations = list(range(0, len(accuracy_history['test_set'])))
+    test_accs = accuracy_history['test_set']
+    validation_accs = accuracy_history['validation_set']
+
+    plt.subplot(2, 1, 1)
+    plt.plot(iterations, test_accs, 'o-')
+    plt.title('Accuracies')
+    plt.ylabel('test accuracy')
+
+    plt.subplot(2, 1, 2)
+    plt.plot(iterations, validation_accs, '.-')
+    plt.xlabel('iteration')
+    plt.ylabel('validation accuracy')
+
+    plt.show()
+
+    return validation_accs[-1]
+
+
+if __name__ == "__main__":
+    main(tests=TEST_MODE)
